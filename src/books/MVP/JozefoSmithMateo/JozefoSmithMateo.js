fece8d53b35155b104f55953f705d8003f889e18
Jarvis1010 <travis.mair@gmail.com>
2019-03-18 16:53:57 -0600

    Add Joseph smith mathew

--- src/books/MVP/JozefoSmithMateo/JozefoSmithMateo.js
+++ src/books/MVP/JozefoSmithMateo/JozefoSmithMateo.js
@@ -1,6 +1,6 @@
-import { CxapitroUnu } from './1';
+import { CxapitroUnu } from "./1";
 
 export const JozefoSmithMateo = {
-  key: 'Jozefo SMITH - Mateo',
+  key: "Jozefo SMITH - Mateo",
   chapters: [CxapitroUnu],
 };
--- src/books/MVP/MVP.js
+++ src/books/MVP/MVP.js
@@ -1,10 +1,10 @@
-import { Moseo } from './Moseo/Moseo';
-import { Abraham } from './Abraham/Abraham';
-import { JozefoSmithMateo } from './JozefoSmithMateo/JozefoSmithMateo';
-import { JozefoSmithHistorio } from './JozefoSmithHistorio/JozefoSmithHistorio';
+import { Moseo } from "./Moseo/Moseo";
+import { Abraham } from "./Abraham/Abraham";
+import { JozefoSmithMateo } from "./JozefoSmithMateo/JozefoSmithMateo";
+import { JozefoSmithHistorio } from "./JozefoSmithHistorio/JozefoSmithHistorio";
 
 export const MVP = {
-  key: 'Multevalora Perlo',
-  image: require('../../assets/MVP.png'),
-  sections: [Moseo], //[Moseo, Abraham, JozefoSmithMateo, JozefoSmithHistorio],
+  key: "Multevalora Perlo",
+  image: require("../../assets/MVP.png"),
+  sections: [Moseo, JozefoSmithMateo], //[ Abraham, JozefoSmithHistorio],
 };
