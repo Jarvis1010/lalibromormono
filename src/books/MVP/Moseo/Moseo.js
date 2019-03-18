306d87a83705480c6d232815814cd7222aef89b9
Jarvis1010 <travis.mair@gmail.com>
2019-03-18 00:55:10 -0600

    Moses chapter 2

--- src/books/MVP/Moseo/Moseo.js
+++ src/books/MVP/Moseo/Moseo.js
@@ -1,17 +1,17 @@
-import { CxapitroUnu } from './1';
-import { CxapitroDu } from './2';
-import { CxapitroTri } from './3';
-import { CxapitroKvar } from './4';
-import { CxapitroKvin } from './5';
-import { CxapitroSes } from './6';
-import { CxapitroSep } from './7';
-import { CxapitroOk } from './8';
+import { CxapitroUnu } from "./1";
+import { CxapitroDu } from "./2";
+import { CxapitroTri } from "./3";
+import { CxapitroKvar } from "./4";
+import { CxapitroKvin } from "./5";
+import { CxapitroSes } from "./6";
+import { CxapitroSep } from "./7";
+import { CxapitroOk } from "./8";
 
 export const Moseo = {
-  key: 'Moseo',
+  key: "Moseo",
   chapters: [
     CxapitroUnu,
-    // CxapitroDu,
+    CxapitroDu,
     // CxapitroTri,
     // CxapitroKvar,
     // CxapitroKvin,
